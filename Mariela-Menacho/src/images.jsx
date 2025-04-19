5f5ff4ab992986f2b89bb02433dbb10d0e3b5231
aceroman <93836487+aceroman@users.noreply.github.com>
2025-04-18 23:13:15 -0400

    added new picture and quick change of wording of letter

--- Mariela-Menacho/src/images.jsx
+++ Mariela-Menacho/src/images.jsx
@@ -4,9 +4,7 @@ import Mari3 from '../src/assets/pic3.jpg'
 import Mari4 from '../src/assets/pic4.jpg'
 import Mari5 from '../src/assets/pic5.jpg'
 import Mari6 from '../src/assets/pic6.jpg'
-import Mari7 from '../src/assets/pic7.jpg'
 import Mari8 from '../src/assets/pic8.jpg'
-import Mari9 from '../src/assets/pic9.jpg'
 import Mari10 from '../src/assets/pic10.jpg'
 import Mari11 from '../src/assets/pic11.jpg'
 import Mari12 from '../src/assets/pic12.jpg'
@@ -20,5 +18,6 @@ import Mari19 from '../src/assets/pic19.jpg'
 import Mari21 from '../src/assets/pic21.jpg'
 import Mari22 from '../src/assets/pic22.jpg'
 import Mari23 from '../src/assets/pic23.jpg'
+import Mari24 from '../src/assets/pic24.jpg'
 
-export const images = [Mari1, Mari4, Mari3, Mari2, Mari5, Mari6, Mari7, Mari8, Mari9, Mari10, Mari11, Mari12, Mari13, Mari14, Mari15, Mari16, Mari17, Mari18, Mari19, Mari21, Mari22, Mari23]
+export const images = [Mari1, Mari4, Mari3, Mari2, Mari24, Mari5, Mari6, Mari8, Mari10, Mari11, Mari12, Mari13, Mari14, Mari15, Mari16, Mari17, Mari18, Mari19, Mari21, Mari22, Mari23]
