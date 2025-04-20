a8e1edb6dc0a3582641d0df7d06a39bbd4dc1a89
aceroman <93836487+aceroman@users.noreply.github.com>
2025-04-20 17:26:13 -0400

    fixing bugs in hosting

--- Mariela-Menacho/vite.config.js
+++ Mariela-Menacho/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/birthdayGirl/Mariela-Menacho',
+  base: '/birthdayGirl/',
   plugins: [react()],
 })
